de831033af4315252747aa7f9018924e22944fa2
camila0424 <milabs.esp24@gmail.com>
2025-05-08 11:33:41 +0200

    primeros pasos

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+//query.selector
+const taskList = document.querySelector ('.js_task-list');
+const inputTaskFilter =document.querySelector('.js-text-task-filter');
+const buscarBtn = document.querySelector ('.js-btn-filter');
+const agregarBtn = document.querySelector ('.js-btn-add');
+const inputTaskAdd = document.querySelector('.js-text-task-add');
+
+// DATOS
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
+
+//FUNCIONES
+for (const task of tasks) {
+  // pintar la tarea en la lista
+  taskList.innerHTML += `<li><input class="checkBox" type="checkbox" name="checkList"/>${task.name}</li>`
+ 
+  
+}
+const checkBox = document.querySelector('.checkBox');
+
+//EVENTOS
+checkBox.addEventListener('click'() => {
+  
+})
+
+
+//LO QUE EJECUTA LA PÁGINA
