c49ac28f01c7d082d829e1c43ccfef9a350bd90e
Nelicah <candezmr2@gmail.com>
2025-05-13 11:38:52 +0200

    condicional en el fetch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 //query.selector
-const taskList = document.querySelector ('.js_task-list');
-const inputTaskFilter =document.querySelector('.js-text-task-filter');
-const buscarBtn = document.querySelector ('.js-btn-filter');
-const agregarBtn = document.querySelector ('.js-btn-add');
-const inputTaskAdd = document.querySelector('.js-text-task-add');
+const taskList = document.querySelector(".js_task-list");
+const inputTaskFilter = document.querySelector(".js-text-task-filter");
+const buscarBtn = document.querySelector(".js-btn-filter");
+const agregarBtn = document.querySelector(".js-btn-add");
+const inputTaskAdd = document.querySelector(".js-text-task-add");
 
 // DATOS
 /* let tasks = [
@@ -29,44 +29,49 @@ let tasks = [];
   // pintar la tarea en la lista
   taskList.innerHTML += `<li><input id="${task.id}" value="${task.completed}" type="checkbox" class="checkBox" name="checkList"/>${task.name}</li>`
  } */
-const checkBoxes = document.querySelectorAll('.checkBox');
 
-function handleClick (ev) {
-  
-  const isCompleted = ev.currentTarget.checked; // valor booleano de check
+function getUser() {
+  fetch(SERVER_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-  const actualTask = ev.currentTarget.parentNode;// muestra el actual li
+      tasks = data.results;
 
-  if (isCompleted === true) {
-    actualTask.classList.add('tachado');
-  } else {
-    actualTask.classList.remove('tachado');
-  };
+      for (const task of tasks) {
+        console.log(task);
 
-};
+        // pintar la tarea en la lista
+        if (task.completed === true) {
+          taskList.innerHTML += `<li class="tachado"><input id="${task.id}" type="checkbox" class="checkBox" name="checkList" checked/>${task.name}</li>`;
+        } else {
+          taskList.innerHTML += `<li><input id="${task.id}" type="checkbox" class="checkBox" name="checkList"/>${task.name}</li>`;
+        }
+      }
 
-function getUser(){
-fetch(SERVER_URL)
-.then((response) => response.json())
-.then((data) => {
+      //EVENTOS
+      const checkBoxes = document.querySelectorAll(".checkBox");
 
-  tasks = data.results;
-  console.log(data);
-  for (const task of tasks) {
-  // pintar la tarea en la lista
-  taskList.innerHTML += `<li><input id="${task.id}" value="${task.completed}" type="checkbox" class="checkBox" name="checkList"/>${task.name}</li>`
-  
- }
-})
+      checkBoxes.forEach((checkBox) => {
+        checkBox.addEventListener("click", handleClick);
+      });
 
-//Completa el código;
-//Guarda la respuesta obtenida en la variable para el listado de tareas: `tasks`
+      console.log("se va a producir el evento");
+    });
+  console.log("se ha producido el fetch");
 }
 getUser();
-//EVENTOS
-checkBoxes.forEach(checkBox => {
-  checkBox.addEventListener('click', handleClick);
-});
 
+function handleClick(ev) {
+  const isCompleted = ev.currentTarget.checked; // valor booleano de check
+
+  const actualTask = ev.currentTarget.parentNode; // muestra el actual li
+
+  if (isCompleted === true) {
+    actualTask.classList.add("tachado");
+  } else {
+    actualTask.classList.remove("tachado");
+  }
+}
 
 //LO QUE EJECUTA LA PÁGINA
